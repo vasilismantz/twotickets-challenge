2f209f64f30bf69e1d8a14981be7937154e3e38e
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-26 19:31:43 +0100

    :sparkles: Add Pizza model

--- pizzashop/orders/models.py
+++ pizzashop/orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Pizza(models.Model):
+
+    class PizzaSize(models.TextChoices):
+        SMALL = 'S', _('Small')
+        MEDIUM = 'M', _('Medium')
+        LARGE = 'L', _('Large')
+
+    pizza_name = models.CharField(max_length=200)
+    pizza_size = models.CharField(
+        max_length=1,
+        choices=PizzaSize.choices,
+        default=PizzaSize.MEDIUM,
+    )
+
+    def __str__(self):
+        return "%s, %s" % (self.pizza_name, self.pizza_size)
