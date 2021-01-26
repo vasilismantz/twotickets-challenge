ec6e446d81f986674f3034820c3fa50a605ce7f2
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-26 21:38:52 +0100

    :sparkles: Add PizzaType, Pizza & Order models

--- pizzashop/orders/admin.py
+++ pizzashop/orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PizzaType, Pizza, Order
+
+admin.site.site_header = "Pizza Shop Admin"
+admin.site.site_title = "Pizza Shop Admin Area"
+admin.site.index_title = "Welcome to the Pizza Shop admin area"
+
+admin.site.register(PizzaType)
+admin.site.register(Pizza)
+admin.site.register(Order)
