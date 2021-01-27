bf00d59cb76542ee752f0746c81043826d3d2b37
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-01-27 07:25:52 +0100

    :sparkles: Add complete_form & result paths

--- pizzashop/orders/urls.py
+++ pizzashop/orders/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name= "orders"
+app_name = "orders"
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('complete_form', views.completeForm, name="complete-form"),
+    path('result', views.result, name="result")
 ]
